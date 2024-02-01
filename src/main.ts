f6813eea1e71724b09a056e32119ad5176731357
Nguyễn Văn Tự Cường <cuongnvtse160875@gmail.com>
2024-02-01 10:20:55 +0700

    123

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ const bootstrap = async () => {
         {
             transport: Transport.TCP,
             options: {
-                host: "localhost",
+                host: "0.0.0.0",
                 port: 3004,
             },
         },
